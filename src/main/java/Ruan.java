a2db8545dee7224a725fd08ce764c1bec08d0394
jianjunruan <40233106+jianjunruan@users.noreply.github.com>
2024-08-27 19:42:03 +0800

    REF [level 0] : rename Duke & skeletal version

---
+++ src/main/java/Ruan.java
@@ -0,0 +1,16 @@
+public class Ruan {
+    public static void main(String[] args) {
+        // String logo = " ____        _        \n"
+        //         + "|  _ \\ _   _| | _____ \n"
+        //         + "| | | | | | | |/ / _ \\\n"
+        //         + "| |_| | |_| |   <  __/\n"
+        //         + "|____/ \\__,_|_|\\_\\___|\n";
+        // System.out.println("Hello from\n" + logo);
+
+        System.out.println("-----------------------------------");
+        System.out.println("Hello, I'm Ruan\nWhat can I do for you?");
+        System.out.println("-----------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("-----------------------------------");
+    }
+}

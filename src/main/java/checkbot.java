dd2745f7ff11ebee521190ef718d7fdcba9105ef
Lihui <lihui.han.13@gmail.com>
2024-08-27 20:55:36 +0800

    add echo feature

--- src/main/java/checkbot.java
+++ src/main/java/checkbot.java
@@ -1,12 +1,38 @@
+import java.util.Scanner;
+
 public class checkbot {
+    public static String horizontalLine = "--------------------------------------";
+
+    public static void printHello() {
+        System.out.println(horizontalLine + System.lineSeparator() +
+                "Hello! I'm checkbot." + System.lineSeparator() +
+                "What can I do for you? :)" + System.lineSeparator() +
+                horizontalLine);
+    }
+
+    public static void printExit() {
+        System.out.println(horizontalLine + System.lineSeparator() +
+                "Bye. Hope to see you again soon!" + System.lineSeparator() +
+                horizontalLine);
+    }
+
+    public static void printEcho() {
+        String input;
+        Scanner in = new Scanner(System.in);
+        input = in.nextLine();
+
+        if (input.equals("bye")) {
+            printExit();
+        } else {
+            System.out.println(horizontalLine + System.lineSeparator() +
+                    input + System.lineSeparator() +
+                    horizontalLine);
+            printEcho();
+        }
+    }
+
     public static void main(String[] args) {
-        String newEntry = "-----------------------------";
-
-        System.out.println(newEntry + System.lineSeparator() +
-                "Hello! I'm checkbot.\n" +
-                "What can I do for you? :)\n" +
-                newEntry);
-
-        System.out.println("Bye. Hope to see you again soon!\n" + newEntry);
+        printHello();
+        printEcho();
     }
 }

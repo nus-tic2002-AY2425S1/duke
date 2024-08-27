c2a1fe6c4ea1d9d11b5671673d7a603dad527d6c
JarrelBin <jarrelbin97@gmail.com>
2024-08-28 01:11:20 +0800

    level-1

--- src/main/java/Alice.java
+++ src/main/java/Alice.java
@@ -1,17 +1,28 @@
+import java.util.Scanner;
+
 public class Alice {
     public static void main(String[] args) {
+        String separator = "____________________________________________________________\n";
         String intro =
                 "____________________________________________________________\n" +
                 "Hello! I'm Alice, here to make you magically organized!\n" +
                         "How may I help you?\n" +
                 "____________________________________________________________";
-        System.out.println(intro);
 
         String ending =
                 "____________________________________________________________\n" +
                 "Bye. Back to my wonderland!\n" +
                 "____________________________________________________________";
 
+        System.out.println(intro);
+        String line;
+        Scanner input = new Scanner(System.in);
+        line = input.nextLine();
+        while(!line.equals("bye")){
+            System.out.print(separator + line + "\n" + separator);
+            input = new Scanner(System.in);
+            line = input.nextLine();
+        }
         System.out.println(ending);
     }
 }

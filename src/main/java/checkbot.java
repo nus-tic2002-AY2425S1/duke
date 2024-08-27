fd1007dddc175bae74d15a5e7b17a3d5b55ffc8b
Lihui <lihui.han.13@gmail.com>
2024-08-27 20:23:59 +0800

    + rename chatbot + greet and exit

---
+++ src/main/java/checkbot.java
@@ -0,0 +1,12 @@
+public class checkbot {
+    public static void main(String[] args) {
+        String newEntry = "-----------------------------";
+
+        System.out.println(newEntry + System.lineSeparator() +
+                "Hello! I'm checkbot.\n" +
+                "What can I do for you? :)\n" +
+                newEntry);
+
+        System.out.println("Bye. Hope to see you again soon!\n" + newEntry);
+    }
+}

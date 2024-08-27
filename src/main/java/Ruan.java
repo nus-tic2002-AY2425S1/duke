561ae674b4c7274023e11aae8f62bc3565391174
jianjunruan <40233106+jianjunruan@users.noreply.github.com>
2024-08-27 20:11:46 +0800

    FEAT [leavel 1] : echo user inputs

--- src/main/java/Ruan.java
+++ src/main/java/Ruan.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Ruan {
     public static void main(String[] args) {
         // String logo = " ____        _        \n"
@@ -7,10 +8,25 @@ public class Ruan {
         //         + "|____/ \\__,_|_|\\_\\___|\n";
         // System.out.println("Hello from\n" + logo);
 
-        System.out.println("-----------------------------------");
-        System.out.println("Hello, I'm Ruan\nWhat can I do for you?");
-        System.out.println("-----------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("-----------------------------------");
+        String line = "";
+        try(Scanner userInput = new Scanner(System.in)){
+
+            System.out.println("  -----------------------------------");
+            System.out.println("  Hello, I'm Ruan\n  What can I do for you?");
+            System.out.println("  -----------------------------------");
+
+            while(!line.trim().toLowerCase().equals("bye")){
+                line = userInput.nextLine();
+                String lineOut = line;
+                if(line.trim().toLowerCase().equals("bye")){
+                    lineOut = "Bye. Hope to see you again soon!";
+                }
+
+                System.out.println("  -----------------------------------");
+                System.out.println("  "+lineOut);
+                System.out.println("  -----------------------------------");
+                
+            }
+        }
     }
 }

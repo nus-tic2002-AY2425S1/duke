845a25ea29f67d856bb36661ea872842ddd9fbf1
JarrelBin <jarrelbin97@gmail.com>
2024-08-28 00:49:51 +0800

    level-0

---
+++ src/main/java/Alice.java
@@ -0,0 +1,17 @@
+public class Alice {
+    public static void main(String[] args) {
+        String intro =
+                "____________________________________________________________\n" +
+                "Hello! I'm Alice, here to make you magically organized!\n" +
+                        "How may I help you?\n" +
+                "____________________________________________________________";
+        System.out.println(intro);
+
+        String ending =
+                "____________________________________________________________\n" +
+                "Bye. Back to my wonderland!\n" +
+                "____________________________________________________________";
+
+        System.out.println(ending);
+    }
+}

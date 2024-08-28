79d029e707e88cdbe08ae4f7616472c4c1f31d22
Lihui <lihui.han.13@gmail.com>
2024-08-29 03:58:41 +0800

    add mark as done feature

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,21 @@
 public class Task {
-    private final String description;
+    protected String description;
+    protected boolean isDone;
 
     public Task(String description) {
         this.description = description;
+        this.isDone = false;
     }
 
     public String getDescription() {
         return description;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
     }
 }
--- src/main/java/checkbot.java
+++ src/main/java/checkbot.java
@@ -5,6 +5,14 @@ public class checkbot {
     public static Task[] tasks = new Task[100];
     public static int taskCount = 0;
 
+    public static String readInput() {
+        String input;
+        Scanner in = new Scanner(System.in);
+        input = in.nextLine();
+
+        return input;
+    }
+
     public static void printHello() {
         System.out.println(horizontalLine + System.lineSeparator() +
                 "Hello! I'm checkbot." + System.lineSeparator() +
@@ -18,14 +26,6 @@ public class checkbot {
                 horizontalLine);
     }
 
-    public static void printTasks() {
-        System.out.println(horizontalLine);
-        for (int i = 0; i < taskCount; i++) {
-            System.out.println(i+1 + ". " + tasks[i].getDescription());
-        }
-        System.out.println(horizontalLine);
-    }
-
     public static void addTask(String description) {
         tasks[taskCount] = new Task(description);
         taskCount++;
@@ -34,12 +34,30 @@ public class checkbot {
                 horizontalLine);
     }
 
-    public static String readInput() {
-        String input;
-        Scanner in = new Scanner(System.in);
-        input = in.nextLine();
+    public static void printTasks() {
+        System.out.println(horizontalLine);
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println(i+1 + ". [" + tasks[i].getStatusIcon() + "] " +
+                    tasks[i].getDescription());
+        }
+        System.out.println(horizontalLine);
+    }
 
-        return input;
+    public static void markTask(int taskNum) {
+        // TODO: exception when taskNum is 0 or > taskCount
+        tasks[taskNum-1].setDone(true);
+        System.out.println("Nice! I've marked this task as done: " + System.lineSeparator() +
+                "  [" + tasks[taskNum-1].getStatusIcon() + "] " + tasks[taskNum-1].getDescription() + System.lineSeparator() +
+                horizontalLine);
+    }
+
+    public static void unmarkTask(int taskNum) {
+        // TODO: exception when taskNum is 0 or > taskCount
+        tasks[taskNum-1].setDone(false);
+        System.out.println("Okay, I've marked this task as not done yet: " + System.lineSeparator() +
+                "  [" + tasks[taskNum-1].getStatusIcon() + "] " + tasks[taskNum-1].getDescription() + System.lineSeparator() +
+                horizontalLine);
     }
 
     public static void main(String[] args) {
@@ -53,6 +71,10 @@ public class checkbot {
                 break;
             } else if (input.trim().equalsIgnoreCase("list")) {
                 printTasks();
+            } else if (input.toLowerCase().startsWith("mark")) {
+                markTask(Integer.parseInt(input.substring(5)));
+            } else if (input.toLowerCase().startsWith("unmark")) {
+                unmarkTask(Integer.parseInt(input.substring(7)));
             } else {
                 addTask(input);
             }

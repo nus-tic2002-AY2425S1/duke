8ee4a873a92bf73baa90058aaf34238ec37ac76b
darrenchew123 <darren09chew99@gmail.com>
2024-08-31 18:15:12 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/DukeGPT.java
@@ -0,0 +1,11 @@
+public class DukeGPT {
+    public static void main(String[] args) {
+        String chatbotName = "DukeGPT";
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm " + chatbotName);
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}

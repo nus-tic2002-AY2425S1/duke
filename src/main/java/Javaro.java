5bed703d4c016c0a8d59302e07366440c022bfd7
Amelia Chua <ameliacly2@gmail.com>
2024-08-31 17:13:28 +0800

    Add Level 0

---
+++ src/main/java/Javaro.java
@@ -0,0 +1,14 @@
+public class Javaro {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        System.out.println(line);
+
+        String chatbotName = "Javaro";
+        System.out.println("Hello! I'm " + chatbotName);
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}

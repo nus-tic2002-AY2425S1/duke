7fe1f550df2228d5bdc7947e806d648d99c3ab18
Denny Hee <e0895780@u.nus.edu>
2024-08-31 21:18:25 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Denny.java
@@ -0,0 +1,13 @@
+public class Denny {
+    public static void main(String[] args) {
+        // Greet the user
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Denny");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        // Exit message
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}

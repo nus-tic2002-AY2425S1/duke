afb67ff49bd2c66409b491b17099db1a4fe49aa4
darrenchew123 <darren09chew99@gmail.com>
2024-08-31 18:31:38 +0800

    Level 1. Echo

--- src/main/java/DukeGPT.java
+++ src/main/java/DukeGPT.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class DukeGPT {
     public static void main(String[] args) {
         String chatbotName = "DukeGPT";
+        Scanner scanner = new Scanner(System.in);
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm " + chatbotName);
         System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        while (true){
+            String userInput = scanner.nextLine();
+            if(userInput.equalsIgnoreCase("bye")){
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(userInput);
+            System.out.println("____________________________________________________________");
+        }
         System.out.println("____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");

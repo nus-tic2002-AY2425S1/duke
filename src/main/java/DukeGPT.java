ffd0bf0aa1cae555a8a390b82a4ddba67caec9eb
darrenchew123 <darren09chew99@gmail.com>
2024-08-31 18:56:44 +0800

    Level 2. Add, List

--- src/main/java/DukeGPT.java
+++ src/main/java/DukeGPT.java
@@ -1,25 +1,58 @@
 import java.util.Scanner;
 
 public class DukeGPT {
-    public static void main(String[] args) {
-        String chatbotName = "DukeGPT";
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm " + chatbotName);
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
+    private static String chatbotName = "DukeGPT";
+    private static String[] task = new String[100];
+    private static int countTask = 0;
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        printGreetings(chatbotName);
         while (true){
             String userInput = scanner.nextLine();
             if(userInput.equalsIgnoreCase("bye")){
                 break;
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(userInput);
-            System.out.println("____________________________________________________________");
+            else if(userInput.equalsIgnoreCase("list")){
+                printList();
+            }
+            else {
+                printAddedItems(userInput);
+            }
         }
+        printGoodbye();
+    }
+
+    public static void printGreetings(String chatbotName){
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm " + chatbotName);
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+    }
+    public static void printGoodbye(){
         System.out.println("____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
+
+    public static void printUserInput(String userInput){
+        System.out.println("____________________________________________________________");
+        System.out.println(userInput);
+        System.out.println("____________________________________________________________");
+    }
+    public static void printAddedItems(String userInput){
+        System.out.println("____________________________________________________________");
+        System.out.println("added: " + userInput);
+        task[countTask] = userInput;
+        countTask++;
+        System.out.println("____________________________________________________________");
+    }
+    public static void printList(){
+        System.out.println("____________________________________________________________");
+        for(int i = 0; i < countTask; i++){
+            System.out.println(i+1 +". " + task[i]);
+        }
+        System.out.println("____________________________________________________________");
+    }
+
 }

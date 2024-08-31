62439f5bf6241fc368fc7897958f45c027de60a9
Denny Hee <e0895780@u.nus.edu>
2024-08-31 21:25:58 +0800

    Level 1. Echo

--- src/main/java/Denny.java
+++ src/main/java/Denny.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Denny {
     public static void main(String[] args) {
         // Greet the user
@@ -6,8 +8,25 @@ public class Denny {
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
+        //Create Scanner
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+
+        while(true) {
+            userInput = scanner.nextLine();
+            if (userInput.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + userInput);
+            System.out.println("____________________________________________________________");
+        }
+
         // Exit message
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
+
+        //Close scanner
+        scanner.close();
     }
 }

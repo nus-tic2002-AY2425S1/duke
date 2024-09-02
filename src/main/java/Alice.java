a60be8ebbb3b59f9479adfed5199799b73b58853
JarrelBin <jarrelbin97@gmail.com>
2024-09-02 15:36:37 +0800

    level-3

--- src/main/java/Alice.java
+++ src/main/java/Alice.java
@@ -11,7 +11,7 @@ public class Alice {
 
     public static void printTasks(){
         for (int i = 0; i < taskCount; i++){
-            System.out.println((i+1) + ". " + Tasks[i].getDescription());
+            System.out.println((i+1) + "." + Tasks[i].getStatusIcon() + Tasks[i].getDescription());
         }
     }
 
@@ -32,11 +32,38 @@ public class Alice {
         String line;
         Scanner input = new Scanner(System.in);
         line = input.nextLine();
-        while(!line.equals("bye")){
-            switch (line) {
+        String[] instruction = line.split(" ");
+        while(!instruction[0].equals("bye")){
+            switch (instruction[0]) {
                 case "list":
                     System.out.print(separator);
                     printTasks();
+                    System.out.print(separator);
+                    break;
+                case "mark":
+                    if (instruction.length == 1){
+                        System.out.println("Do specify the task number!");
+                        break;
+                    }
+
+                    System.out.print(separator);
+                    Task task_mark = Tasks[Integer.parseInt(instruction[1])-1];
+                    task_mark.setDone(true);
+                    System.out.println("Nice! I've marked this task as done:)");
+                    System.out.println(task_mark.getStatusIcon() + task_mark.getDescription());
+                    System.out.print(separator);
+                    break;
+                case "unmark":
+                    if (instruction.length == 1){
+                        System.out.println("Do specify the task number!");
+                        break;
+                    }
+
+                    System.out.print(separator);
+                    Task task_unmark = Tasks[Integer.parseInt(instruction[1])-1];
+                    task_unmark.setDone(false);
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println(task_unmark.getStatusIcon() + task_unmark.getDescription());
                     System.out.print(separator);
                     break;
                 default:
@@ -46,6 +73,7 @@ public class Alice {
 
             input = new Scanner(System.in);
             line = input.nextLine();
+            instruction = line.split(" ");
         }
         System.out.println(ending);
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,7 +1,18 @@
 public class Task {
     protected String description = "";
+    protected boolean isDone;
+
     public Task(String description) {
         this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X] " : "[ ] "); // mark done task with X
+    }
+
+    public void setDone(boolean b) {
+        this.isDone = b;
     }
     public String getDescription() {
         return description;

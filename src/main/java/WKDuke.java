d30a6a52120ea0cd7d38e7b3d1318f08132d8e72
waikhincheong <waikhin1996@gmail.com>
2024-09-07 15:34:08 +0800

    Enhance user input parsing to support new actions

--- src/main/java/WKDuke.java
+++ src/main/java/WKDuke.java
@@ -7,7 +7,10 @@ public class WKDuke {
     private static final String endBorderLine = "\t____________________________________________________________\n";
     private static final String stringIndent = "\t ";
     private static final String exitKeyword = "bye";
-    private static final String listItemKeyword = "list";
+    private static final String listTaskKeyword = "list";
+    private static final String markTaskDoneKeyword = "mark";
+    private static final String markTaskUndoneKeyword = "unmark";
+    private static List<Task> taskList = new ArrayList<>();
 
     public static void echo(String message) {
         System.out.println(startBorderLine);
@@ -15,10 +18,10 @@ public class WKDuke {
         System.out.println(endBorderLine);
     }
 
-    public static void printList(List<String> list) {
+    public static void printList(List<Task> taskList) {
         System.out.println(startBorderLine);
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(stringIndent + Integer.toString(i + 1) + ". " + list.get(i));
+        for (int i = 0; i < taskList.size(); i++) {
+            System.out.println(stringIndent + Integer.toString(i + 1) + ". " + taskList.get(i));
         }
         System.out.println(endBorderLine);
     }
@@ -39,20 +42,33 @@ public class WKDuke {
         System.out.println(stringIndent + "Hello! I'm WKDuke\n" + stringIndent + "What can I do for you?");
         System.out.println(endBorderLine);
 
-        String input;
-        List<String> list = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
+        String action = "";
 
-        while (true) {
-            input = sc.nextLine();
-            input = input.trim();
-            if (input.equals(exitKeyword)) {
-                break;
-            } else if (input.equals(listItemKeyword)) {
-                printList(list);
-            } else {
-                list.add(input);
-                echo("added: " + input);
+        while (!action.equals(exitKeyword)) {
+            String input = sc.nextLine().trim();
+            if (input.isEmpty()) {
+                continue;
+            }
+
+            String[] inputWords = input.split(" ");
+            action = inputWords[0];
+            switch (action) {
+                case exitKeyword:
+                    break;
+                case listTaskKeyword:
+                    printList(taskList);
+                    break;
+                case markTaskDoneKeyword:
+                    // Mark Task Done
+                    break;
+                case markTaskUndoneKeyword:
+                    // Mark Task Undone
+                    break;
+                default:
+                    taskList.add(new Task(input));
+                    echo("added: " + input);
+                    break;
             }
         }
 

981ba53f83a063e0e0a1a31db9c1355645045ad1
Ian <ian1125757@gmail.com>
2024-09-08 22:32:47 +0800

    Level-2: Add, List

--- src/main/java/Snitch.java
+++ src/main/java/Snitch.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Snitch {
     public static void main(String[] args) {
@@ -12,6 +13,8 @@ public class Snitch {
         // Set up for user input
         Scanner scanner = new Scanner(System.in);
 
+        // Create a list to store the user inputs
+        ArrayList<String> tasks = new ArrayList<>();
         // keep accepting user input
         while (true) {
             // Get input from the user
@@ -22,11 +25,17 @@ public class Snitch {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;  // Exit the loop and terminate
             }
-
-            // Echo the user's input
-            System.out.println(userInput);
+            // Check if the user types "list"
+            if (userInput.equalsIgnoreCase("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
+                }
+            } else {
+                // else add the user input to the list of tasks
+                tasks.add(userInput);
+                System.out.println("added: " + userInput);
+            }
         }
-
         // Close the scanner
         scanner.close();
     }

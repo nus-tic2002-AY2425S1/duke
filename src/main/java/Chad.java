47829b94e435d02d12d6853fbcfad98b82000adc
Wen Fu <e1062715@u.nus.edu>
2024-09-08 19:38:55 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Chad.java
@@ -0,0 +1,11 @@
+public class Chad {
+    public static void main(String[] args) {
+        String logo = " Chad\n";
+        System.out.println("_________________________________________________________________");
+        System.out.println("Hello from " + logo);
+        System.out.println("What can I do for you?\n");
+        System.out.println("_________________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_________________________________________________________________");
+    }
+}

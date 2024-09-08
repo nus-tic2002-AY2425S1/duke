24dada98257ccadfd56fd7ee099c12ff37efa459
Ian <ian1125757@gmail.com>
2024-09-08 21:59:36 +0800

    Level-0: Rename, Greet, Exit

--- src/main/java/Snitch.java
+++ src/main/java/Snitch.java
@@ -1,10 +1,10 @@
-public class Duke {
+public class Snitch {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Snitch!\n" + "What can I do for you?\n" + "Bye. Hope to see you again soon!\n");
     }
 }

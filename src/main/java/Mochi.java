122f527f7238257ca7be2dda48f5c5e5d562720c
bowen <bowen@entiis.com>
2024-09-09 15:52:08 +0800

    added ability echo any input

--- src/main/java/Mochi.java
+++ src/main/java/Mochi.java
@@ -1,12 +1,26 @@
+import java.util.Scanner;
 public class Mochi {
-    final static String name = "Mochi";
-    public static void main(String[] args) {
-        String greating = "____________________________________________________________\n"
-                + " Hello! I'm " + name + "\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n";
-        System.out.println(greating);
+  final static String _name = "Mochi";
+  public static void response(String input) {
+    System.out.println("____________________________________________________________");
+    System.out.println(input);
+    System.out.println("____________________________________________________________");
+  }
+  public static void main(String[] args) {
+    String greating = "____________________________________________________________" + System.lineSeparator()
+      + " Hello! I'm " + _name + System.lineSeparator()
+      + " What can I do for you?";
+    System.out.println(greating);
+    Scanner in = new Scanner(System.in);
+    String line = "";
+    while (!line.equals("bye")) {
+      line = in.nextLine();
+      if (!line.equals("bye")) {
+        response(line);
+      }
+      else {
+        response("Bye. Hope to see you again soon!");
+      }
     }
+  }
 }

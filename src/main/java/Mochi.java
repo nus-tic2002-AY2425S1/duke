f1032bf1dfbc843380e1f64d42bad1009987c6f6
bowen <bowen@entiis.com>
2024-09-09 15:15:54 +0800

    Rename Bot Name, Updated greatings to level0 requirement

---
+++ src/main/java/Mochi.java
@@ -0,0 +1,12 @@
+public class Mochi {
+    final static String name = "Mochi";
+    public static void main(String[] args) {
+        String greating = "____________________________________________________________\n"
+                + " Hello! I'm " + name + "\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(greating);
+    }
+}

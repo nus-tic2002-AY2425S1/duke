0fa291eaaf72a50f58d98372e6db88085f38e8f4
Joseph Lim <e1062711@u.nus.edu>
2024-09-12 15:49:38 +0800

    Level 0 : Replace bot name to JosBot and Change the greetings (Edit JosBot.java)

---
+++ src/main/java/JosBot.java
@@ -0,0 +1,14 @@
+public class JosBot {
+    public static void main(String[] args) {
+
+        String start_txt = "____________________________________________________________\n"
+                +"Hello I'm JosBot\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n";
+        System.out.println(start_txt);
+
+        String end_txt = "Bye. Hope to see you again soon!\n"
+                +"____________________________________________________________\n";
+        System.out.println(end_txt);
+    }
+}

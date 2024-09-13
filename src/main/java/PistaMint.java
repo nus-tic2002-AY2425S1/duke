f88f31f2f1e015a84376df2980b670e24448aa27
LAPTOP-63KBTPI9\kok_k <kaiyin_99@hotmail.com>
2024-09-14 00:30:42 +0800

    Commit Level 2.Add,List

--- src/main/java/PistaMint.java
+++ src/main/java/PistaMint.java
@@ -1,13 +1,14 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PistaMint {
     public static int length = 50;
     public static String line = "-".repeat(length);
     public static String input="";
-
+    public static String[] listItems=new String[100];
+    public static int itemCount=0;
     public static void greetings() {
         System.out.println("\t"+line + "\n\tNi Hao! I'm PistaMint\n\tWhat can I do for you?\n\t" + line);
-
     }
 
     public static void exit() {
@@ -18,7 +19,23 @@ public class PistaMint {
         if(input.equalsIgnoreCase("bye")){
             return;
         }
-        System.out.println("\t"+line + "\n\t" + input + "\n\t" + line);
+        System.out.println("\t"+line+"\n\tadded: "+input+"\n\t"+line);
+    }
+    public static String[] addList(String item){
+        listItems[itemCount]=item;
+        itemCount++;
+        return Arrays.copyOf(listItems,itemCount);
+    }
+
+    public static void printItems(){
+        System.out.println("\t"+line);
+        for(int i=0;i<listItems.length;i++){
+            if(listItems[i]==null){
+                break;
+            }
+            System.out.println("\t"+i+". "+listItems[i]);
+        }
+        System.out.println("\t"+line+"\n");
     }
 
     public static void main(String[] args) {
@@ -26,7 +43,14 @@ public class PistaMint {
         while (!input.equalsIgnoreCase("bye")) {
             Scanner in = new Scanner(System.in);
             input = in.nextLine();
-            echo(input);
+            if(input.equalsIgnoreCase("list")) {
+                printItems();
+            }
+            else{
+                echo(input);
+                addList(input);
+            }
+
         }
         exit();
     }

cdc1b8fb08f01429d028d94e891a3e845f661a55
LAPTOP-63KBTPI9\kok_k <kaiyin_99@hotmail.com>
2024-09-14 00:05:13 +0800

    Commit for Level-1 Echo

--- src/main/java/PistaMint.java
+++ src/main/java/PistaMint.java
@@ -1,15 +1,33 @@
+import java.util.Scanner;
+
 public class PistaMint {
-    public static int length=50;
-    public static String line="-".repeat(length);
-    public static void greetings(){
-        System.out.println(line+"\nNi Hao! I'm PistaMint\nWhat can I do for you?\n"+line);
+    public static int length = 50;
+    public static String line = "-".repeat(length);
+    public static String input="";
+
+    public static void greetings() {
+        System.out.println("\t"+line + "\n\tNi Hao! I'm PistaMint\n\tWhat can I do for you?\n\t" + line);
 
     }
-    public static void exit(){
-        System.out.println("Xie Xie! Hope to see you again soon~\n"+line);
+
+    public static void exit() {
+        System.out.println("\t"+line+"\n\tXie Xie! Hope to see you again soon~\n\t" + line);
     }
+
+    public static void echo(String input) {
+        if(input.equalsIgnoreCase("bye")){
+            return;
+        }
+        System.out.println("\t"+line + "\n\t" + input + "\n\t" + line);
+    }
+
     public static void main(String[] args) {
         greetings();
+        while (!input.equalsIgnoreCase("bye")) {
+            Scanner in = new Scanner(System.in);
+            input = in.nextLine();
+            echo(input);
+        }
         exit();
     }
 }

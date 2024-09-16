cd41022f4bfc852108231354bf4c8439e3d9a0c7
Wen Fu <e1062715@u.nus.edu>
2024-09-16 16:47:42 +0800

    level 2 add list

--- src/main/java/Chad.java
+++ src/main/java/Chad.java
@@ -1,27 +1,63 @@
 import java.util.Scanner;
 
+
+
 public class Chad {
+
+    public static void chadSay(String chadSay)
+    {
+        String myline = "_________________________________________________________________";
+        System.out.println(myline);
+        System.out.println(chadSay);
+        System.out.println(myline);
+        return;
+    }
+
+    public static void printList(String[] inputList){
+
+        // TODO: add your code here
+       String myline = "_________________________________________________________________";
+       //System.out.println("List:");
+            
+        System.out.println(myline);
+        for(int i=0;i<99;i++)
+        {
+            if(inputList[i]==null)
+            {
+                break;
+            }
+            System.out.println((i+1)+"."+inputList[i]);
+        }
+        System.out.println(myline);
+    }
+
     public static void main(String[] args) {
         String myline = "_________________________________________________________________";
         String logo = " Chad\n";
-        System.out.println(myline);
-        System.out.println("Hello from " + logo);
-        System.out.println("What can I do for you?\n");
-        System.out.println(myline);
-
+        chadSay("Hello from " + logo+"\n"+"What can I do for you?\n");
+        String[] inputList = new String[100];
         String line;
+        int i=0;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
         while(!line.equals("bye"))
         {
-            System.out.println(myline);
-            System.out.println("You said: " + line);
-            System.out.println(myline);
+
+            if(line.equals("list"))
+            {
+                printList(inputList);
+                line=in.nextLine();
+                continue;
+            }
+
+            chadSay("added: " + line);
+
+            inputList[i]=line;
+            i++;
             line=in.nextLine();
+            
         }
         
-
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(myline);
+        chadSay("Bye. Hope to see you again soon!");
     }
 }

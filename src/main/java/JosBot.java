f80e32554492e9836afb023aba24d8bb87ec286d
Joseph Lim <e1062711@u.nus.edu>
2024-09-17 14:51:24 +0800

    added Echo (Level - 1)

--- src/main/java/JosBot.java
+++ src/main/java/JosBot.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
 public class JosBot {
     public static void main(String[] args) {
-
+        String user_input = "";
         String start_txt = "____________________________________________________________\n"
                 +"Hello I'm JosBot\n"
                 + "What can I do for you?\n"
                 + "____________________________________________________________\n";
         System.out.println(start_txt);
+
+        while(!user_input.equals("bye"))
+        {
+
+            Scanner in = new Scanner(System.in);
+            user_input = in.nextLine();
+            String mid_txt = "____________________________________________________________\n"
+                    +user_input+"\n"
+                    + "____________________________________________________________\n";
+            System.out.println(mid_txt);
+        }
 
         String end_txt = "Bye. Hope to see you again soon!\n"
                 +"____________________________________________________________\n";

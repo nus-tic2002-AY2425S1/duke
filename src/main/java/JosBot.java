27e0ba63f316672f1bec51f930efc6b8c9fd091a
Joseph Lim <e1062711@u.nus.edu>
2024-09-18 05:35:23 +0800

    Level 3 - Mark as Done

--- src/main/java/JosBot.java
+++ src/main/java/JosBot.java
@@ -1,44 +1,65 @@
 import java.util.Scanner;
 
 public class JosBot {
+
+    public static void printDash(){
+        System.out.print("____________________________________________________________\n");
+    }
+
     public static void main(String[] args) {
-        String user_input = "";
+
         String start_txt = "____________________________________________________________\n"
                 +"Hello I'm JosBot\n"
                 + "What can I do for you?\n"
                 + "____________________________________________________________\n";
         System.out.println(start_txt);
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int list_count = 0;
+        Scanner in = new Scanner(System.in);
+        String user_input = in.nextLine();;
 
         while(!user_input.equals("bye"))
         {
-            Scanner in = new Scanner(System.in);
-            user_input = in.nextLine();
+            printDash();
+           // Scanner in = new Scanner(System.in);
+
             if(user_input.equals("list"))
             {
-                System.out.println("____________________________________________________________\n");
-
                 for(int i = 1; i < list_count+1; i++)
                 {
-                    System.out.println( i+ ". "+list[i-1]);
+                    System.out.println( i+ ". ["+list[i-1].getStatusIcon()+"] "+list[i-1].getDescription());
                 }
-                System.out.println("____________________________________________________________\n");
+            }
+            else if(user_input.contains("mark"))
+            {
+                String last_word = user_input.substring(user_input.length()-1);
+                Integer last_digit = Integer.valueOf(last_word) - 1;
+
+                if(user_input.startsWith("mark"))
+                {
+                    System.out.println("Nice! I've marked this task as done:");
+                    list[last_digit].markAsDone();
+                }
+                else if(user_input.startsWith("unmark"))
+                {
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    list[last_digit].markAsNotDone();
+                }
+                System.out.println("["+list[last_digit].getStatusIcon()+"] "+list[last_digit].getDescription());
             }
             else
             {
-                String mid_txt = "____________________________________________________________\n"
-                        +"added: "+user_input+"\n"
-                        + "____________________________________________________________\n";
-                System.out.println(mid_txt);
-                list[list_count] = user_input;
+                System.out.println("added: "+user_input);
+                Task t = new Task(user_input);
+                list[list_count] = t;
                 list_count++;
             }
-
+            printDash();
+            user_input = in.nextLine();
         }
 
-        String end_txt = "Bye. Hope to see you again soon!\n"
-                +"____________________________________________________________\n";
+        String end_txt = "Bye. Hope to see you again soon!\n";
+        printDash();
         System.out.println(end_txt);
     }
 }

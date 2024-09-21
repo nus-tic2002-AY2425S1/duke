d9c9273a9848b4b1a8b38f6bfbe8c90e3f354559
ZHIQIANG-00 <e0895861@u.nus.edu>
2024-09-21 15:13:37 +0800

    edit the code in todo, event, deadline and May. so that when wrong command being input. respectrive error message will be display

--- src/main/java/May.java
+++ src/main/java/May.java
@@ -18,84 +18,113 @@ public class May {
         System.out.println("What can I do for you?");
 
         while(true){
-            // Scan for user input
-            input = scan.nextLine();
+            try {
+                // Scan for user input
+                input = scan.nextLine();
 
-            // split the input into two put one for task status other part for description
-            String[] command = input.split(" ",2);
+                // split the input into two put one for task status other part for description
+                String[] command = input.split(" ", 2);
 
-            // Check for Exit command & output exit command message
-            if(command[0].equalsIgnoreCase("bye")){
-                /*Exit Message*/
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            }
-
-            // Add a normal task (not used in the updated design, can be removed)
-            else if (command[0].equalsIgnoreCase("add") && command.length > 1) {
-                addTask(new Task(command[1]));
-            }
-
-            // Check for taskList Command & output item inside taskList
-            else if (command[0].equalsIgnoreCase("taskList")){
-                displayTaskList();
-            }
-
-            // Mark the task as done
-            else if (command[0].equalsIgnoreCase("tick")){
-                int taskIndex = Integer.parseInt(command[1])-1;
-                // Check for taskIndex is within the task number
-                // If it is within the range, then we mark as done
-                if (taskIndex < listNum){
-                   taskList[taskIndex].markAsDone();
-                   System.out.println("Congratulations! You have completed the task! ");
-                   System.out.println(" " + taskList[taskIndex]);
+                // Check for Exit command & output exit command message
+                if (command[0].equalsIgnoreCase("bye")) {
+                    /*Exit Message*/
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
                 }
-            }
 
-            // Mark the task as cross since it has not done
-            else if (command[0].equalsIgnoreCase("cross")){
-                int taskIndex = Integer.parseInt(command[1])-1;
-                // Check for taskIndex is within the task number
-                // If it is within the range, then we mark as done
-                if (taskIndex < listNum){
-                    taskList[taskIndex].unmarkAsDone();
-                    System.out.println("Task have not done, Kindly complete the task. ");
-                    System.out.println(" " + taskList[taskIndex]);
+                // Add a normal task (not used in the updated design, can be removed)
+                else if (command[0].equalsIgnoreCase("add") && command.length > 1) {
+                    addTask(new Task(command[1]));
                 }
-            }
 
-            //Create the todo Task and output number of task in the list
-            else if (command[0].equalsIgnoreCase("todo") && command.length > 1) {
-                String description = command[1];
-                addOrUpdateTask(new ToDo(description));
-            }
+                // Check for taskList Command & output item inside taskList
+                else if (command[0].equalsIgnoreCase("taskList")) {
+                    displayTaskList();
+                }
 
-            // Mark the deadline of the task
-            else if (command[0].equalsIgnoreCase("deadline") && command.length > 1) {
-                String[] doneBy = command[1].split("/by ", 2);
-                if (doneBy.length > 1) {
+                // Mark the task as done
+                else if (command[0].equalsIgnoreCase("tick")) {
+                    int taskIndex = Integer.parseInt(command[1]) - 1;
+                    // Check for taskIndex is within the task number
+                    // If it is within the range, then we mark as done
+                    if (taskIndex < listNum) {
+                        taskList[taskIndex].markAsDone();
+                        System.out.println("Congratulations! You have completed the task! ");
+                        System.out.println(" " + taskList[taskIndex]);
+                    }
+                    else {
+                        // add in error handle
+                        throw new ErrorException("Invalid task index. Please enter a valid task index.");
+                    }
+                }
+
+                // Mark the task as cross since it has not done
+                else if (command[0].equalsIgnoreCase("cross")) {
+                    int taskIndex = Integer.parseInt(command[1]) - 1;
+                    // Check for taskIndex is within the task number
+                    // If it is within the range, then we mark as done
+                    if (taskIndex < listNum) {
+                        taskList[taskIndex].unmarkAsDone();
+                        System.out.println("Task have not done, Kindly complete the task. ");
+                        System.out.println(" " + taskList[taskIndex]);
+                    }
+                    else {
+                        // add in error handle
+                        throw new ErrorException("Invalid task index. Please enter a valid task index.");
+                    }
+                }
+
+                // Create "todo-task"
+                else if (command[0].equalsIgnoreCase("todo")) {
+                    if (command.length < 2 || command[1].trim().isEmpty()) {
+                        throw new ErrorException(" 'todo' command cannot be empty.");
+                    }
+                    addOrUpdateTask(new ToDo(command[1]));
+                }
+
+                // Mark the deadline of the task
+                else if (command[0].equalsIgnoreCase("deadline")) {
+                    if (command.length < 2) {
+                        throw new ErrorException("'deadline' command cannot be empty.");
+                    }
+                    String[] doneBy = command[1].split("/by ", 2);
+                    if (doneBy.length < 2) {
+                        throw new ErrorException("Invalid deadline format.");
+                    }
                     addOrUpdateTask(new Deadline(doneBy[0].trim(), doneBy[1].trim()));
-                } else {
-                    System.out.println("Invalid deadline.");
                 }
-            }
 
-            // Create upcoming event task
-            else if (command[0].equalsIgnoreCase("event") && command.length > 1) {
-                String[] eventTime = command[1].split("/from | /to ",3);
-                if (eventTime.length > 2) {
+                // Create upcoming event task
+                else if (command[0].equalsIgnoreCase("event")) {
+                    if (command.length < 2) {
+                        throw new ErrorException("'event' command cannot be empty.");
+                    }
+                    String[] eventTime = command[1].split("/from | /to ", 3);
+                    if (eventTime.length < 3) {
+                        throw new ErrorException("Invalid event format.");
+                    }
                     addOrUpdateTask(new Event(eventTime[0].trim(), eventTime[1].trim(), eventTime[2].trim()));
                 }
-                else{
-                    System.out.println("Invalid event.");
+
+                // Unknown command
+                else {
+                    throw new Error("I'm Sorry, I don't recognize that command. ");
                 }
             }
 
-
-            else {
-                System.out.println("Unknown command");
+            // Handle custom error
+            catch (ErrorException e){
+                System.out.println("OOPS!!! " + e.getMessage());
             }
+            //Handle tick and cross index error
+            catch (NumberFormatException e){
+                System.out.println("Please enter a valid number.");
+            }
+            //Handle other unexpected error
+            catch (Exception e){
+                System.out.println("Something went wrong. Please try again.");
+            }
+
         }
         // Close Scan
         scan.close();
@@ -115,7 +144,7 @@ public class May {
         addTask(newTask);
     }
 
-    // Add task to the tasklist
+    // Add task to the task-list
     private static void addTask(Task task){
         taskList[listNum] = task;
         listNum++;

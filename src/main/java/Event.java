9c04c7bcdbac49a86d1afaaffd5b2654bc665960
ZHIQIANG-00 <e0895861@u.nus.edu>
2024-10-20 23:42:53 +0800

    edit event class to hand specifi time in HH:MM format

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,16 +1,20 @@
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Event extends Task{
-    protected LocalDate from;
-    protected LocalDate to;
-    private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("MMM dd yyyy");
+    protected LocalDateTime from;
+    protected LocalDateTime to;
+    //private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("MMM dd yyyy");
+
+    // Define a common DateTimeFormatter to maintain consistency
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+    private static final DateTimeFormatter OUTPUT_FORMATTER = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm");
 
     // Construct an event task with schedule timing
-    public Event(String taskName, String from, String to){
+    public Event(String taskName, LocalDateTime from, LocalDateTime to){
         super(taskName);
-        this.from = LocalDate.parse(from);
-        this.to = LocalDate.parse(to);
+        this.from = from;
+        this.to = to;
     }
 
     @Override
@@ -21,12 +25,12 @@ public class Event extends Task{
     // output the event to string with [E] and the schedule timeline for event from till to.
     @Override
     public String toString(){
-        return "[" + getTaskType() + "][" + getStatusIcon() + "] " + taskName + " (from: " + from.format(OUTPUT_FORMAT) + " to: " + to.format(OUTPUT_FORMAT) + ")";
+        return "[" + getTaskType() + "][" + getStatusIcon() + "] " + taskName + " (from: " + from.format(OUTPUT_FORMATTER) + " to: " + to.format(OUTPUT_FORMATTER) + ")";
     }
 
     @Override
     public String toFileString() {
-        return getTaskType() + " | " + (isDone ? "1" : "0") + " | " + taskName + " | " + from + " | " + to;
+        return getTaskType() + " | " + (isDone ? "1" : "0") + " | " + taskName + " | " + from.format(FORMATTER) + " | " + to.format(FORMATTER);
     }
 
 }

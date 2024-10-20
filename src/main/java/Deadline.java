dfcaf18d6e15944e5353b97a26e20e4bb9910157
ZHIQIANG-00 <e0895861@u.nus.edu>
2024-10-20 23:41:58 +0800

    edit Dealine class so that it is able to load or save timing as well in HH:mm format. as current only can save date

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,15 +1,19 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Deadline extends Task{
 
 
     // Add in Deadline variable (Change to used local date)
-    protected LocalDate by;
+    protected LocalDateTime by;
 
-    public Deadline(String taskName, String by){
+    // Define the date time format as a constant
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+
+    public Deadline(String taskName, LocalDateTime by){
         super(taskName);
-        this.by = LocalDate.parse(by);
+        this.by = by;
     }
 
     @Override
@@ -19,12 +23,12 @@ public class Deadline extends Task{
 
     @Override
     public String toString() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
-        return "[" + getTaskType() + "][" + getStatusIcon() + "] " + taskName + " (by: " + by.format(formatter) + ")";
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm");
+        return "[" + getTaskType() + "][" + getStatusIcon() + "] " + taskName + " (by: " + by.format(outputFormatter) + ")";
     }
 
     @Override
     public String toFileString() {
-        return getTaskType() + " | " + (isDone ? "1" : "0") + " | " + taskName + " | " + by;
+        return getTaskType() + " | " + (isDone ? "1" : "0") + " | " + taskName + " | " + by.format(FORMATTER);
     }
 }

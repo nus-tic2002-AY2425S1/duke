b82bd66c3216f0ca7e73ed062349eb80c4621865
Lihui <lihui.han.13@gmail.com>
2024-10-22 21:10:05 +0800

    test run JUnit

---
+++ src/test/java/checkbot/CheckbotTest.java
@@ -0,0 +1,11 @@
+package checkbot;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CheckbotTest {
+    @Test
+    public void dummyTest() {
+        assertEquals(2,2);
+    }
+}

79759ba821c39a69145c3d4536aac3253294d05b
BrittoBabu <brittobabu2000@gmail.com>
2024-11-03 01:42:25 +0800

    update event class to handle date-time format

--- src/main/java/starkchatbot/taskmanager/Event.java
+++ src/main/java/starkchatbot/taskmanager/Event.java
@@ -2,22 +2,31 @@ package starkchatbot.taskmanager;
 
 public class Event extends Task {
 
-    private String starkAt;
+    private String startAt;
     private String endBy;
+    private String readableStartAt;
+    private String readableEndAt;
 
     public Event(String task, String startAt, String endBy) {
-        super(task + " (from: " + startAt + " to: " + endBy + ")");
-        this.starkAt = startAt;
+        super(task );
+        this.startAt = startAt;
         this.endBy = endBy;
+        this.readableStartAt = DateTimeParser.parseDateTime(startAt);
+        this.readableEndAt = DateTimeParser.parseDateTime(endBy);
     }
 
     @Override
     public String printTask() {
-        return "[E] " + super.printTask();
+        return "[E] " + super.printTask() + " (from: " + readableStartAt + " to: " + readableEndAt + ")";
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + " (from: " + startAt + " to: " + endBy + ")";
     }
 
     public String getStarkAt() {
-        return starkAt;
+        return startAt;
     }
     public String getEndBy() {
         return endBy;

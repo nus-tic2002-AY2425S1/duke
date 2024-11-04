67a3567f2ff758421f2255f0cb34ee433ac9b5fe
Amelia Chua <ameliacly2@gmail.com>
2024-11-04 21:21:21 +0800

    Add JavaDoc comments to the common package

---
+++ src/main/java/common/Constants.java
@@ -0,0 +1,18 @@
+package common;
+
+/**
+ * A utility class that contains various constant values used throughout the application.
+ * 
+ * <p>
+ * The Constants class centralizes commonly used string constants to ensure consistency and ease of maintenance across the application. 
+ * This design allows for easy modification of constants without changing the logic of the application.
+ * </p>
+ */
+// to put magic literals
+public class Constants {
+    public static final String SPACE = " ";
+    public static final String EMPTY_STRING = "";
+    public static final String OPEN_SQUARE_BRACKET = "[";
+    public static final String CLOSE_SQUARE_BRACKET = "]";
+    public static final String ENCODE_TASK_SEPARATOR = " | ";
+}
--- src/main/java/common/Messages.java
+++ src/main/java/common/Messages.java
@@ -1,5 +1,13 @@
 package common;
 
+/**
+ * A utility class that contains various message constants used throughout the application.
+ * 
+ * <p>
+ * The Messages class centralizes all the strings used for user feedback, error messages, and prompts to ensure consistency and ease of maintenance. 
+ * This design allows for easy modification of messages without changing the logic of the application.
+ * </p>
+ */
 public class Messages {
     
     public static final String SPACE = " ";
@@ -62,13 +70,4 @@ public class Messages {
     public static final String ERROR_INVALID_DATETIME_FORMAT = ERROR + "Invalid date/time format";
     public static final String ERROR_END_BEFORE_START = ERROR + "Start date/time must be before end date/time";
 
-
-    // public static final String MESSAGE_INIT_FAILED = "Failed to initialise address book application. Exiting...";
-    // public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    // public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
-    // public static final String MESSAGE_PERSON_NOT_IN_ADDRESSBOOK = "Person could not be found in address book";
-    // public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
-    // public static final String MESSAGE_PROGRAM_LAUNCH_ARGS_USAGE = "Launch command format: "
-    //         + "java seedu.addressbook.Main [STORAGE_FILE_PATH]";
-    // public static final String MESSAGE_USING_STORAGE_FILE = "Using storage file : %1$s";
 }

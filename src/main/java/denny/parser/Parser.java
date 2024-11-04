88f8b7d5260bad3ece02ea4fc52815f54258dad7
Denny Hee <e0895780@u.nus.edu>
2024-11-04 23:00:17 +0800

    Week 7: A-Enums

--- src/main/java/denny/parser/Parser.java
+++ src/main/java/denny/parser/Parser.java
@@ -8,40 +8,50 @@ import denny.exception.DennyException;
  * Supports various commands including list, mark, unmark, todo, deadline, event, delete, and bye.
  */
 public class Parser {
-    /**
-     * Parses a user input string into a Command object.
-     * @param fullCommand The complete command string from user input
-     * @return A Command object corresponding to the user's input
-     * @throws DennyException if the command is invalid or cannot be parsed
-     */
-    public Command parse(String fullCommand) throws DennyException {
-        String[] parts = fullCommand.split(" ", 2);
-        String commandWord = parts[0].toLowerCase();
-        String arguments = parts.length > 1 ? parts[1] : "";
 
-        switch (commandWord) {
-            case "list":
-                return new ListCommand();
-            case "mark":
-                return new MarkCommand(arguments);
-            case "unmark":
-                return new UnmarkCommand(arguments);
-            case "todo":
-                return new AddTodoCommand(arguments);
-            case "deadline":
-                return new AddDeadlineCommand(arguments);
-            case "event":
-                return new AddEventCommand(arguments);
-            case "delete":
-                return new DeleteCommand(arguments);
-            case "bulk-delete":
-                return new BulkDeleteCommand(arguments);
-            case "find":
-                return new FindCommand(arguments);
-            case "bye":
-                return new ExitCommand();
-            default:
-                throw new DennyException("Unknown command: " + commandWord);
+    public enum CommandType {
+        LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, BULK_DELETE, FIND, BYE;
+
+        // Converts a string command to a CommandType, handling case sensitivity
+        public static CommandType fromString(String command) throws DennyException {
+            try {
+                return CommandType.valueOf(command.toUpperCase());
+            } catch (IllegalArgumentException e) {
+                throw new DennyException("Unknown command: " + command);
+            }
         }
     }
+
+    public Command parse(String fullCommand) throws DennyException {
+        String[] parts = fullCommand.split(" ", 2);
+        String arguments = parts.length > 1 ? parts[1] : "";
+
+        CommandType commandType = CommandType.fromString(parts[0]);
+
+        switch (commandType) {
+            case LIST:
+                return new ListCommand();
+            case MARK:
+                return new MarkCommand(arguments);
+            case UNMARK:
+                return new UnmarkCommand(arguments);
+            case TODO:
+                return new AddTodoCommand(arguments);
+            case DEADLINE:
+                return new AddDeadlineCommand(arguments);
+            case EVENT:
+                return new AddEventCommand(arguments);
+            case DELETE:
+                return new DeleteCommand(arguments);
+            case BULK_DELETE:
+                return new BulkDeleteCommand(arguments);
+            case FIND:
+                return new FindCommand(arguments);
+            case BYE:
+                return new ExitCommand();
+            default:
+                throw new DennyException("Unknown command: " + parts[0]);
+        }
+    }
+
 }

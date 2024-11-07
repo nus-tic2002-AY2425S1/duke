0201b90c5ac7f8e83d72c4f7256dd2f80ff9cd09
Kok Kai Li <kaili-98@hotmail.com>
2024-11-07 17:09:31 +0800

    Level 0: Rename, Greet, Exit

---
+++ src/main/java/KLbot.java
@@ -0,0 +1,18 @@
+public class KLbot {
+    public static void main(String[] args) {
+        String botName = "KLbot";
+
+        printLine();
+        System.out.println("Hello! I'm " + botName + ".");
+        System.out.println("What can I do for you?");
+
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+
+        printLine();
+    }
+
+    public static void printLine() {
+        System.out.println("____________________________________________________________");
+    }
+}

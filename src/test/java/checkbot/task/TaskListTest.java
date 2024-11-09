e093617b4cdc5934f7137f5d1fefb06a3acead7a
Lihui <lihui.han.13@gmail.com>
2024-11-09 18:35:38 +0800

    add addEvent JUnit tests

--- src/test/java/checkbot/task/TaskListTest.java
+++ src/test/java/checkbot/task/TaskListTest.java
@@ -39,19 +39,22 @@ public class TaskListTest {
 
     @Test
     public void testAddDeadline_emptyInput() {
-        Throwable exception = assertThrows(InvalidInputException.class, () -> TaskList.addDeadline("/by 28/10/2024 2340"));
+        Throwable exception = assertThrows(InvalidInputException.class, () ->
+                TaskList.addDeadline("/by 28/10/2024 2340"));
         assertEquals(exception.getMessage(),Messages.EMPTY_DESCRIPTION);
     }
 
     @Test
     public void testAddDeadline_emptyTime() {
-        Throwable exception = assertThrows(InvalidInputException.class, () -> TaskList.addDeadline("Deadline description /by"));
+        Throwable exception = assertThrows(InvalidInputException.class, () ->
+                TaskList.addDeadline("Deadline description /by"));
         assertEquals(exception.getMessage(),Messages.EMPTY_TIME);
     }
 
     @Test
     public void testAddDeadline_commandNotFound() {
-        Throwable exception = assertThrows(CommandNotFoundException.class, () -> TaskList.addDeadline("Deadline description by 28/10/2024 2340"));
+        Throwable exception = assertThrows(CommandNotFoundException.class, () ->
+                TaskList.addDeadline("Deadline description by 28/10/2024 2340"));
         assertEquals(exception.getMessage(),Messages.DEADLINE_ERROR);
     }
 
@@ -64,9 +67,12 @@ public class TaskListTest {
 
     @Test
     public void testAddDeadline_invalidDate_idxOOB() {
-        assertThrows(IndexOutOfBoundsException.class, () -> TaskList.addDeadline("Deadline description /by 14102024"));
-        assertThrows(IndexOutOfBoundsException.class, () -> TaskList.addDeadline("Deadline description /by 14 oct"));
-        assertThrows(IndexOutOfBoundsException.class, () -> TaskList.addDeadline("Deadline description /by 14 october 2024"));
+        assertThrows(IndexOutOfBoundsException.class, () ->
+                TaskList.addDeadline("Deadline description /by 14102024"));
+        assertThrows(IndexOutOfBoundsException.class, () ->
+                TaskList.addDeadline("Deadline description /by 14 oct"));
+        assertThrows(IndexOutOfBoundsException.class, () ->
+                TaskList.addDeadline("Deadline description /by 14 october 2024"));
     }
 
     @Test
@@ -74,5 +80,70 @@ public class TaskListTest {
         assertThrows(DateTimeException.class, () -> TaskList.addDeadline("Deadline description /by 12/25/2025"));
         assertThrows(DateTimeException.class, () -> TaskList.addDeadline("Deadline description /by 25/12/2025 2400"));
         assertThrows(DateTimeException.class, () -> TaskList.addDeadline("Deadline description /by 31/2/2024 1500"));
+    }
+
+    @Test
+    public void testAddEvent() {
+        try {
+            Event event = TaskList.addEvent("Event description /from 28/10/2024 2340 /to 29/10/2024 0500");
+            assertEquals("Event description", event.getDescription());
+            assertEquals(LocalDateTime.of(2024,10,28,23,40), event.startDateTime);
+            assertEquals(LocalDateTime.of(2024,10,29,5,0), event.endDateTime);
+            assertEquals(TaskStatus.NOT_DONE, event.status);
+            assertEquals(TaskPriority.NOT_SET, event.priority);
+        } catch (Exception e) {
+            System.out.println("Exception Caught");
+        }
+    }
+
+    @Test
+    public void testAddEvent_emptyInput() {
+        Throwable exception = assertThrows(InvalidInputException.class, () ->
+                TaskList.addEvent("/from 28/10/2024 2340 /to 29/10/2024 0500"));
+        assertEquals(exception.getMessage(),Messages.EMPTY_DESCRIPTION);
+    }
+
+    @Test
+    public void testAddEvent_emptyTime() {
+        Throwable exception = assertThrows(InvalidInputException.class, () ->
+                TaskList.addEvent("Event description /from /to"));
+        assertEquals(exception.getMessage(),Messages.EMPTY_TIME);
+    }
+
+    @Test
+    public void testAddEvent_commandNotFound() {
+        Throwable exception = assertThrows(CommandNotFoundException.class, () ->
+                TaskList.addEvent("Event description from 28/10/2024 2340 to 29/10/2024 0500"));
+        assertEquals(exception.getMessage(),Messages.EVENT_ERROR);
+    }
+
+    @Test
+    public void testAddEvent_invalidDate_numberFormat() {
+        assertThrows(NumberFormatException.class, () ->
+                TaskList.addEvent("Event description /from 14oct /to 28oct"));
+        assertThrows(NumberFormatException.class, () ->
+                TaskList.addEvent("Event description /from 14oct2024 /to 28oct2024"));
+        assertThrows(NumberFormatException.class, () ->
+                TaskList.addEvent("Event description /from 14-10-2024 /to 28-10-2024"));
+    }
+
+    @Test
+    public void testAddEvent_invalidDate_idxOOB() {
+        assertThrows(IndexOutOfBoundsException.class, () ->
+                TaskList.addEvent("Event description /from 14102024 /to 28102024"));
+        assertThrows(IndexOutOfBoundsException.class, () ->
+                TaskList.addEvent("Event description /from 14 oct /to 28 oct"));
+        assertThrows(IndexOutOfBoundsException.class, () ->
+                TaskList.addEvent("Event description /from 14 october 2024 /to 28 october 2024"));
+    }
+
+    @Test
+    public void testAddEvent_dateTimeException() {
+        assertThrows(DateTimeException.class, () ->
+                TaskList.addEvent("Event description /from 12/25/2025 /to 12/28/2025"));
+        assertThrows(DateTimeException.class, () ->
+                TaskList.addEvent("Event description /from 25/12/2025 2400 /to 28/12/2025 0100"));
+        assertThrows(DateTimeException.class, () ->
+                TaskList.addEvent("Event description /from 31/2/2024 1500 /to 31/4/2024 1000"));
     }
 }

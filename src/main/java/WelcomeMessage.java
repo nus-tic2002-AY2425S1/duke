47ddf6928840e684b113042136a8b8cab024d0af
ZHIQIANG-00 <e0895861@u.nus.edu>
2024-11-12 20:30:51 +0800

    remove imoji from welcome message

--- src/main/java/WelcomeMessage.java
+++ src/main/java/WelcomeMessage.java
@@ -7,8 +7,8 @@ public class WelcomeMessage {
         System.out.println("    \\  /| (_) | (_) |__| ");
         System.out.println("     \\/  \\___/ \\___/(_) ");
         System.out.println();
-        System.out.println("⚔ Greetings, Seeker of Order! I am May, Guardian of Tasks ⚔");
+        System.out.println(" Greetings, Seeker of Order! I am May, Guardian of Tasks ");
         System.out.println(" Entrust your tasks to me, and we shall bring them to completion! ");
-        System.out.println("What tasks shall we conquer today?");
+        System.out.println(" What tasks shall we conquer today? ");
     }
 }

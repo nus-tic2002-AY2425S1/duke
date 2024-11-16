63441ba693509a53eb70856241f800782f28ce80
Dancepop <songnian123@hotmail.com>
2024-11-17 05:24:53 +0800

    chore: add reference storage, fix storage volume (#16)
    * add reference
    
    * add directory creation
    
    ---------
    
    Co-authored-by: Tay Song Nian <e1062207@u.nus.edu>

--- src/main/java/duke/dancepop/utils/Storage.java
+++ src/main/java/duke/dancepop/utils/Storage.java
@@ -18,6 +18,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+// Referenced from
+// https://stackoverflow.com/questions/58313994/how-to-write-into-the-same-csv-file-with-filewriter
+// https://www.javaguides.net/2019/07/java-read-file-with-filesreadalllines-api.html
 public class Storage {
     private static final String FILE_PATH = "volume/";
     private static final String DEFAULT_FILE_NAME = "data.csv";
@@ -31,6 +34,7 @@ public class Storage {
      * @param fileName The name of the file to save the tasks.
      */
     public static void saveToFile(String fileName) {
+        ensureDirectoryExists();
         Path filePath = Paths.get(FILE_PATH, fileName);
         Log.printMsg("Saving tasks to " + filePath);
 
@@ -51,6 +55,8 @@ public class Storage {
         } catch (IOException ioe) {
             Log.printMsg("Error occurred while saving file: ", ioe.getMessage());
         }
+
+        Log.printMsg("Finished saving tasks to " + filePath);
     }
 
     /**
@@ -97,6 +103,7 @@ public class Storage {
         } catch (IOException | FileException ioe) {
             Log.printMsg(ioe.getMessage());
         }
+        Log.printMsg("Finished loading data from " + filePath);
     }
 
     /**
@@ -170,5 +177,17 @@ public class Storage {
 
     private static void createEmptyCsvFile(FileWriter writer) throws IOException {
         writer.append("").flush();
+    }
+
+    private static void ensureDirectoryExists() {
+        // Referenced from https://howtodoinjava.com/java/io/how-to-check-if-file-exists-in-java/#:~:text=To%20test%20to%20see%20if,and%20otherwise%20does%20not%20exists.
+        Path directoryPath = Paths.get(FILE_PATH);
+        if (!Files.exists(directoryPath)) {
+            try {
+                Files.createDirectories(directoryPath);
+            } catch (IOException e) {
+                Log.printMsg("Error creating directory: ", e.getMessage());
+            }
+        }
     }
 }

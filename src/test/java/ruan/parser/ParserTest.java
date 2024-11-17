825be9fdfe6bdd6595842447ce8ba82d7d4ee750
jianjunruan <40233106+jianjunruan@users.noreply.github.com>
2024-11-17 19:08:47 +0800

    Feat [level 8] : add gradle test

---
+++ src/test/java/ruan/parser/ParserTest.java
@@ -0,0 +1,99 @@
+package ruan.parser;
+
+import ruan.command.*;
+import ruan.exception.RuanException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ParserTest {
+
+    @Test
+    public void parse_validCommand_returnsCorrectCommandForReadBook() throws RuanException {
+        Command command = Parser.parse("todo Read book");
+        assertTrue(command instanceof AddCommand);
+    }
+
+    @Test
+    public void parse_validCommand_returnsCorrectCommandForBorrowBook() throws RuanException {
+        Command command = Parser.parse("todo Borrow book");
+        assertTrue(command instanceof AddCommand);
+    }
+
+    @Test
+    public void parse_validCommand_returnsCorrectCommandForReturnBook() throws RuanException {
+        Command command = Parser.parse("deadline Return book /by 2024-11-17");
+        assertTrue(command instanceof AddCommand);
+    }
+
+    @Test
+    public void parse_validCommand_returnsCorrectCommandForSubmitProjectString() throws RuanException {
+        Command command = Parser.parse("deadline Submit Project /by Sun 2024-11-17");
+        assertTrue(command instanceof AddCommand);
+    }
+
+    @Test
+    public void parse_validCommand_returnsCorrectCommandForSubmitProjectDateTime() throws RuanException {
+        Command command = Parser.parse("deadline Submit Project /by 2024-11-17 2359");
+        assertTrue(command instanceof AddCommand);
+    }
+
+    @Test
+    public void parse_validCommand_returnsCorrectCommandForExamString() throws RuanException {
+        Command command = Parser.parse("event Exam /from Sat 2024-11-30 /to sat 2024-11-30");
+        assertTrue(command instanceof AddCommand);
+    }
+
+    @Test
+    public void parse_validCommand_returnsCorrectCommandForExamDateTime() throws RuanException {
+        Command command = Parser.parse("event Exam /from 2024-11-30 0900 /to 2024-11-30 1100");
+        assertTrue(command instanceof AddCommand);
+    }
+
+    @Test
+    public void parse_validCommand_returnsCorrectCommandForExamDate() throws RuanException {
+        Command command = Parser.parse("event Exam /from 2024-11-30 /to 2024-11-30");
+        assertTrue(command instanceof AddCommand);
+    }
+
+    @Test
+    public void parse_deleteCommand_returnsDeleteCommand() throws RuanException {
+        Command command = Parser.parse("delete 1");
+        assertTrue(command instanceof DeleteCommand);
+    }
+
+    @Test
+    public void parse_markCommand_returnsSetDoneCommand() throws RuanException {
+        Command command = Parser.parse("mark 2");
+        assertTrue(command instanceof SetDoneCommand);
+    }
+
+    @Test
+    public void parse_unmarkCommand_returnsSetDoneCommand() throws RuanException {
+        Command command = Parser.parse("unmark 3");
+        assertTrue(command instanceof SetDoneCommand);
+    }
+    
+    @Test
+    public void parse_listCommand_returnsListCommand() throws RuanException {
+        Command command = Parser.parse("list");
+        assertTrue(command instanceof ListCommand);
+    }
+    
+    @Test
+    public void parse_invalidCommand_throwsException() {
+        assertThrows(RuanException.class, () -> Parser.parse("invalid command"));
+    }
+
+    @Test
+    public void parse_emptyInput_throwsException() {
+        assertThrows(RuanException.class, () -> Parser.parse(" "));
+    }
+
+    @Test
+    public void parse_exitCommand_returnsExitCommand() throws RuanException {
+        Command command = Parser.parse("bye");
+        assertTrue(command instanceof ExitCommand);
+    }
+}
---
+++ src/test/java/ruan/task/TaskListTest.java
@@ -0,0 +1,78 @@
+package ruan.task;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ruan.exception.RuanException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TaskListTest {
+
+    private TaskList taskList;
+
+    @BeforeEach
+    public void setUp() {
+        taskList = new TaskList();
+    }
+
+    @Test
+    public void addTask_validTask_taskAdded() {
+        Task task = new Todo("Write code");
+        taskList.addTask(task);
+
+        assertEquals(1, taskList.size());
+        assertEquals("[T][ ] Write code", taskList.getTaskDescription(0));
+    }
+
+    @Test
+    public void deleteTask_validIndex_taskDeleted() throws RuanException {
+        Task task = new Todo("Write code");
+        taskList.addTask(task);
+        taskList.deleteTask(0);
+
+        assertEquals(0, taskList.size());
+    }
+
+    @Test
+    public void deleteTask_invalidIndex_throwsException() {
+        assertThrows(RuanException.class, () -> taskList.deleteTask(0));
+    }
+
+    @Test
+    public void setDone_markTask_taskMarkedAsDone() throws RuanException {
+        Task task = new Todo("Write code");
+        taskList.addTask(task);
+        taskList.setDone(0, true);
+
+        assertEquals("[T][X] Write code", taskList.getTaskDescription(0));
+    }
+
+    @Test
+    public void setDone_unmarkTask_taskMarkedAsNotDone() throws RuanException {
+        Task task = new Todo("Write code");
+        taskList.addTask(task);
+        taskList.setDone(0, true);
+        taskList.setDone(0, false);
+
+        assertEquals("[T][ ] Write code", taskList.getTaskDescription(0));
+    }
+
+    @Test
+    public void setDone_invalidIndex_throwsException() {
+        assertThrows(RuanException.class, () -> taskList.setDone(0, true));
+    }
+
+    @Test
+    public void getTaskDescription_validIndex_returnsDescription() {
+        Task task = new Todo("Write code");
+        taskList.addTask(task);
+
+        assertEquals("[T][ ] Write code", taskList.getTaskDescription(0));
+    }
+
+    @Test
+    public void getTaskDescription_invalidIndex_throwsException() {
+        assertThrows(IndexOutOfBoundsException.class, () -> taskList.getTaskDescription(0));
+    }
+}
